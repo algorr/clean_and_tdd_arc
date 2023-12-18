c3ebbc474786da536b1f9b58ecb7ea507a87bf2e
Ümit Kar <umitkar08@gmail.com>
2023-12-18 11:50:22 +0300

    app .env files ghosted

---
+++ lib/product/init/config/index.dart
@@ -0,0 +1,4 @@
+export 'app_configuration.dart';
+export 'app_environment.dart';
+export 'env_dev.dart';
+export 'env_prod.dart';
--- lib/product/init/main_app_initialize.dart
+++ lib/product/init/main_app_initialize.dart
@@ -4,7 +4,6 @@ import 'package:clean_arc/product/init/config/app_environment.dart';
 import 'package:clean_arc/product/init/config/env_dev.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:easy_logger/easy_logger.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:kartal/kartal.dart';
@@ -14,14 +13,13 @@ import 'package:logger/logger.dart';
 final class MainAppInitialize {
   /// The function sets up a zone and initializes a logger with a pretty printer.
   Future<void> setup() async {
+    WidgetsFlutterBinding.ensureInitialized();
     await runZonedGuarded<Future<void>>(_initialize, (error, stack) {
       Logger(printer: PrettyPrinter());
     });
   }
 
   Future<void> _initialize() async {
-    BindingBase.debugZoneErrorsAreFatal = false;
-    WidgetsFlutterBinding.ensureInitialized();
     await EasyLocalization.ensureInitialized();
     EasyLocalization.logger.enableLevels = [LevelMessages.error];
     await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
